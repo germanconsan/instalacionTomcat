9764ca17ca50d2566caeca9acdf69b237c696b64
Germán <germanconsan@gmail.com>
2024-02-11 12:08:37 +0100

    .

--- codedeploy-scripts/beforeinstall.sh
+++ codedeploy-scripts/beforeinstall.sh
@@ -7,7 +7,7 @@ apt-get install -y unzip
 # Descargar e instalar Apache Tomcat
 mkdir /opt/tomcat
 cd /opt/tomcat
-wget [URL_DEL_TOMCAT] -O tomcat.zip
+wget https://downloads.apache.org/tomcat/tomcat-9/v9.0.56/bin/apache-tomcat-9.0.56.zip -O tomcat.zip
 unzip tomcat.zip
 mv apache-tomcat-*/* /opt/tomcat/
 rm -rf apache-tomcat-*

57337bcc8b9528a67def3d2c8b1c090510e09b2f
Germán <germanconsan@gmail.com>
2024-02-11 17:26:02 +0100

    defenitiva v1

--- codedeploy-scripts/afterinstall.sh
+++ codedeploy-scripts/afterinstall.sh
@@ -3,8 +3,5 @@
 # Detener Tomcat antes de copiar los archivos compilados
 /opt/tomcat/bin/shutdown.sh
 
-# Copiar los archivos compilados al directorio de despliegue de Tomcat
-cp -r /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/libs/* /opt/tomcat/webapps/
-
 # Iniciar Tomcat
 /opt/tomcat/bin/startup.sh
